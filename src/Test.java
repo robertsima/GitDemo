4782e9c0217b02818517f186756ceafc553372c1
Rahafba <rbarakat@ggc.edu>
2020-10-09 11:07:43 -0400

    committing new method from Dr. B

--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!!!!");
 		System.out.println("Hello world from Nick");
+		System.out.println("Hello World from Dr.B");
 	}
 
 }
