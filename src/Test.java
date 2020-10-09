618a51f03fa08e0d7215d74e7f5865522a823a11
nporter1 <nporter1@ggc.edu>
2020-10-09 11:19:03 -0400

    changing first method from Nick

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!!!!");
+		System.out.println("Hello World to demo merge conflict from Nick!!!!");
 		System.out.println("Hello world from Nick");
 		System.out.println("Hello World from Dr.B");
 		System.out.println("Hello World from Greatzel!!!! :> ");
